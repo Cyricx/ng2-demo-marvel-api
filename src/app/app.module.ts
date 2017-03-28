c843a8048b5674701fb8b22fc6255c412061a621
Cyricx <contact@chrisbyram.com>
2017-03-28 18:18:25 -0500

    2. Angular Pieces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+/*
+  Components - building blocks of an application (custom html elements)
+  Services - Data retrieval/persistance for the application
+  Pipes - Data formatting (ONLY), should not be used to
+    modify the data for another piece to used
+  Directives - Direct html modifications (typically through custom html attributes)
+  Module - Sub sections of the application or cross-application reusable pieces (plugins)
+  Enum/Interface/Class - Generic code structures
+  Guards - Used with routing to control path resolutions, typically used to deliver 404 for invalid ids
+    or for authorization on a route
+*/
+
 @NgModule({
   declarations: [
     AppComponent
